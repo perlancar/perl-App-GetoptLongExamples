5d8a504099a4b01115042074381258c6814c7dfd
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-30 19:12:14 +0700

    Initial commit

---
+++ lib/App/GetoptLongExamples.pm
@@ -0,0 +1,20 @@
+package App::GetoptLongExamples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Collection of CLI examples related to Getopt::Long
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Getopt::Long>
+
+=cut
